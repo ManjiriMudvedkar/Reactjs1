26f0c92992de96263bf8b636ef7e6a3108a2ad9c
Manjiri <30858627+ManjiriMudvedkar@users.noreply.github.com>
2022-12-09 12:00:24 +0530

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,15 +4,17 @@ provider "aws" {
 
 resource "aws_s3_bucket" "react-aws-terraform-github-actions-s3-bucket" {
   bucket = var.bucket_name
-  acl = "public-read"
   policy = data.aws_iam_policy_document.s3-website-policy.json
-
-  ##versioning {
-   # enabled = var.versioning
- #}
+  bucket_prefix = var.bucket_prefix
+  acl = var.acl
   
- # tags = var.tags
+   versioning {
+    enabled = var.versioning
+  }
+  
+  tags = var.tags
 }
+  
 
 
 
